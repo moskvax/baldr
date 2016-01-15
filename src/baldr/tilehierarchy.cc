b36179fcb68eefbc4b2a30d1ba961cb7c3290780
Kevin Kreiser <kevinkreiser@gmail.com>
2016-01-15 15:22:10 -0500

    make subdivisions in tile configurable

--- src/baldr/tilehierarchy.cc
+++ src/baldr/tilehierarchy.cc
@@ -5,7 +5,7 @@ using namespace valhalla::midgard;
 namespace valhalla {
 namespace baldr {
 TileHierarchy::TileLevel::TileLevel(const boost::property_tree::ptree& pt):
-  tiles({{-180, -90}, {180, 90}}, pt.get<float>("size")){
+  tiles({{-180, -90}, {180, 90}}, pt.get<float>("size"), pt.get<unsigned short>("subdivisions", 1)){
   level = pt.get<uint8_t>("level");
   name = pt.get<std::string>("name");
   //if not provided default to everything
